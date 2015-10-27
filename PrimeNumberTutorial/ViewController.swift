091573ef0e3b89575e33db45860f7bc0191c153c
thefreshduke <scottyshaw1@gmail.com>
2015-10-27 16:52:29 -0400

    Added functionality to check if input number is prime

--- PrimeNumberTutorial/ViewController.swift
+++ PrimeNumberTutorial/ViewController.swift
@@ -15,11 +15,34 @@ class ViewController: UIViewController {
     
     @IBAction func checkIfNumberIsPrime(sender: AnyObject) {
         let number = Int(textField.text!)
+        
+        if number <= 1 {
+            label.text = "NO"
+            return
+        }
+        
+        var isNumberPrime: Bool = true
+        
+        // O(n)
+        for var i: Int = 2; i < number; i++ {
+            if number! % i == 0 {
+                isNumberPrime = false
+                break
+            }
+        }
+        
+        if isNumberPrime {
+            label.text = "YES"
+        }
+        else {
+            label.text = "NO"
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "Is this a prime number?"
     }
 
     override func didReceiveMemoryWarning() {
