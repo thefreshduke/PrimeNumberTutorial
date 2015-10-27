4f230ef9a78cbf1002c25d4a0d2fe69f29ddb69b
thefreshduke <scottyshaw1@gmail.com>
2015-10-27 16:27:17 -0400

    Added text field, label, and button; set test alignments to centered; created IBOutlets and IBAction

---
+++ PrimeNumberTutorial/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PrimeNumberTutorial
+//
+//  Created by Scotty Shaw on 10/27/15.
+//  Copyright © 2015 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func checkIfNumberIsPrime(sender: AnyObject) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
