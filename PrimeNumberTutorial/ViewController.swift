2ee94185e0972d3ec91fc7817da7c806c5ef1935
thefreshduke <scottyshaw1@gmail.com>
2015-10-27 16:29:39 -0400

    Created number variable and set it equal to number in inputField by unwrapping inputField.text and casting the string to an integer

--- PrimeNumberTutorial/ViewController.swift
+++ PrimeNumberTutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     
     @IBAction func checkIfNumberIsPrime(sender: AnyObject) {
+        let number = Int(textField.text!)
     }
 
     override func viewDidLoad() {
